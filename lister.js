1f5bb59c1414aa12ee106fe44ba14c521c762f9e
Jeremiah Barth <jbarth1986@gmail.com>
2020-07-02 17:12:45 -0500

    Added item strike-through and deletion functionality

--- lister.js
+++ lister.js
@@ -7,6 +7,7 @@ else {
 
 function ready() {
   addSubmitListener()
+  addDeleteListener()
 }
 
 function addSubmitListener() {
@@ -16,9 +17,15 @@ function addSubmitListener() {
 
 function addItem() {
   let newItem = document.getElementById("newItem")
-  let list = document.getElementById("grocery-list")
-  let li = document.createElement('li')
+  let list = document.getElementById("groceryList")
+  let li = document.createElement("li")
+  let checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.name = "select"
+  checkbox.classList.add("checkbox-delete")
+  checkbox.addEventListener('click', checkboxListener)
 
+  li.appendChild(checkbox)
   li.appendChild(document.createTextNode(newItem.value))
   list.appendChild(li)
 
@@ -26,7 +33,31 @@ function addItem() {
 }
 
 function enterItem(e) {
-  if(event.keyCode === 13) {
+  if(e.keyCode === 13) {
     document.getElementById("addButton").click()
   }
+}
+
+function checkboxListener(e) {
+  let checkbox = e.target
+  if(checkbox.checked) {
+    checkbox.parentElement.style.textDecoration = "line-through"
+  }
+  else {
+    checkbox.parentElement.style.textDecoration = "none"
+  }
+}
+
+function addDeleteListener() {
+  document.getElementById("removeButton").addEventListener("click", deleteItems)
+}
+
+function deleteItems() {
+  let checkBoxes = Array.from(document.getElementsByClassName("checkbox-delete"))
+
+  checkBoxes.forEach(checkbox => {
+    if(checkbox.checked) {
+      checkbox.parentElement.remove()
+    }
+  })
 }
